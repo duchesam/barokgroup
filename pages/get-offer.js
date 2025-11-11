e43070b398ed2075baf37ff40ba9affea10437df
duchesam <78324845+duchesam@users.noreply.github.com>
2025-11-10 22:01:52 -0500

    Update get-offer.js

--- pages/get-offer.js
+++ pages/get-offer.js
@@ -9,7 +9,7 @@ export default function GetOffer(){
       <Header />
       <main className="container content">
         <h1>Get a Cash Offer</h1>
-        <p>Email <a href="mailto:hello@barokgroup.com">hello@barokgroup.com</a> with your property address and timeline, or call <a href="tel:+15712753720">(571) 275-3720</a>. We’ll reply quickly.</p>
+        <p>Email <a href="mailto:hello@barokgroup.org">hello@barokgroup.org</a> with your property address and timeline, or call <a href="tel:+15712753720">(571) 275-3720</a>. We’ll reply quickly.</p>
       </main>
       <Footer />
     </>
