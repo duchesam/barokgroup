ddd31ba222d4bcf011144106d39bf43ea11fe1c7
duchesam <78324845+duchesam@users.noreply.github.com>
2025-12-19 12:59:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,17 @@
-// pages/_app.js
 import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  const siteName = "Baruch-Ermi LLC";
-  const siteUrl = "https://barokgroup.org"; // keep your domain
-  const email = "admin@baruchermi.org";
-  const phoneDisplay = "(904) 478-9306";
-
+export default function Home() {
   return (
     <>
       <Head>
-        {/* Basic SEO defaults */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="application-name" content={siteName} />
-        <meta name="author" content={siteName} />
-        <meta name="robots" content="index,follow" />
-
-        {/* Open Graph defaults */}
-        <meta property="og:site_name" content={siteName} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={siteUrl} />
-
-        {/* Trust signals */}
-        <meta name="contact" content={`${email} | ${phoneDisplay}`} />
+        <title>Barok Group</title>
+        <meta name="description" content="Barok Group website" />
       </Head>
 
-      <Component {...pageProps} />
+      <main style={{ padding: 24 }}>
+        <h1>Welcome</h1>
+        <p>Site is deploying correctly on Vercel.</p>
+      </main>
     </>
   );
 }
